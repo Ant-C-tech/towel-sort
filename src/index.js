57bc4d9852200b348392fe024491cb9278454393
Anton Chyvikov <chivikovanton@gmail.com>
2021-01-28 17:25:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+     if (matrix && matrix.length != 0) {
+        let sortDirection = "straight";
+        let result = [];
+        matrix.forEach((element) => {
+            if (sortDirection === "straight") {
+                result = result.concat(element);
+                sortDirection = "reverse";
+            } else if (sortDirection === "reverse") {
+                result = result.concat(element.reverse());
+                sortDirection = "straight";
+            }
+        });
+        return result;
+    } else {
+        return [];
+    }
 }
